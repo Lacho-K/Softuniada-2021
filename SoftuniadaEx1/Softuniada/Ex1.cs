76ea7d811369c256e713ad3cd5c3d2491a84bf42
Lacho123 <44599728+Lacho123@users.noreply.github.com>
2021-08-03 14:27:55 +0300

    Add files via upload

---
+++ SoftuniadaEx1/Softuniada/Ex1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Softuniada
+{
+    class Ex1
+    {
+        static void Main(string[] args)
+        {
+            int lowerBound = int.Parse(Console.ReadLine());
+            int upperBound = int.Parse(Console.ReadLine());
+            int primeNumCount = 0;
+            for (int i = lowerBound; i <= upperBound; i++)
+            {
+                if (CheckPrime(i))
+                {
+                    primeNumCount++;
+                    Console.Write(i + " ");
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine($"The total number of prime numbers between {lowerBound} to {upperBound} is {primeNumCount}");
+        }
+
+        public static bool CheckPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            int boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+                if (number % i == 0)
+                    return false;
+
+            return true;
+        }
+    }
+}
